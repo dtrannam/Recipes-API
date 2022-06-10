5a7e90c3a89b1462e2ad306fc32c62db7d09d1d6
David Trannam <trannamd@oregonstate.edu>
2022-06-09 22:54:22 -0700

    adding postgre

---
+++ app/core/management/commands/wait_for_db.py
@@ -0,0 +1,9 @@
+"""
+Commands to wait for DB to be avaiable
+"""
+from django.core.management.base import BaseCommand
+
+class Command(BaseCommand):
+
+    def handle(self, *arg, **options):
+        pass
